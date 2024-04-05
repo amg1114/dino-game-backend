74f5f83e541e09de37e26eb7553e230fe1e34ba0
Alejandro Moreno <alejandromogil@gmail.com>
2024-04-04 21:00:43 -0500

    Project Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { DataSourceConfig } from './config/data.source';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(DataSourceConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import * as morgan from 'morgan';
+
+import { CORS } from './config/constants/cors';
+
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+
+  app.use(morgan(configService.get('ENVIRONMENT', 'combined')));
+  app.setGlobalPrefix('api');
+  app.enableCors(CORS);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+  await app.listen(configService.get('PORT', 3000));
 }
 bootstrap();
