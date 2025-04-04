d119865b2a88e63eb559d28be1ab4c61692141d8
Felipe Cabal <phelipekabbal_85@hotmail.com>
2025-04-04 12:04:27 -0500

    fix: adjust swagger docs, fix Reports found dto

--- src/reports/dto/responses/found.dto.ts
+++ src/reports/dto/responses/found.dto.ts
@@ -3,17 +3,25 @@ import { Report } from 'src/reports/entities/report.entity';
 
 export class ReportsFoundDto {
   @ApiProperty({
-    example: Report,
+    example: [
+      {
+        id: 1,
+        state: 'PENDING',
+        user: { id: 1, name: 'John Doe' },
+        videoGame: { id: 1, title: 'Game Title' },
+        typeReport: { id: 1, name: 'Bug' },
+      },
+    ],
   })
   data: Report[];
 
   @ApiProperty({
-    example: '2',
+    example: 2,
   })
   offset: number;
 
   @ApiProperty({
-    example: '10',
+    example: 10,
   })
   total: number;
 }
