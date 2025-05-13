40a914f5f56dc3b7054616377e4a2d439fc44cbe
Alejandro Moreno <alejandromogil@gmail.com>
2025-05-12 20:44:26 -0500

    refactor: update getAdminStatistics method to calculate current month and year dynamically

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Req, Sse, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, Sse, UseGuards } from '@nestjs/common';
 import { from, interval, map, mergeMap, startWith } from 'rxjs';
 import { StatisticsService } from './statistics.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -31,13 +31,12 @@ export class StatisticsController {
     status: 500,
     description: 'Internal Server Error',
   })
-  @Sse(':month/:year')
-  async getAdminStatistics(
-    @Param('month')
-    month: string,
-    @Param('year') year: string,
-    @Req() req: any,
-  ) {
+  @Sse()
+  async getAdminStatistics(@Req() req: any) {
+    const currentDate = new Date();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const year = String(currentDate.getFullYear());
+
     return interval(1000).pipe(
       startWith(0), // 🔥 Emite inmediatamente un primer valor
       mergeMap(() =>
@@ -49,6 +48,37 @@ export class StatisticsController {
           ),
         ).pipe(map((statistics) => ({ data: statistics }))),
       ),
+    );
+  }
+
+  @Roles(Role.ADMINISTRATOR, Role.DEVELOPER)
+  @ApiOperation({
+    summary: 'Get statistics for a specific month and year',
+    description:
+      'This endpoint provides statistics for a specific month and year.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Statistics data',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  @Get(':month/:year')
+  async getStatistics(
+    @Param('month') month: string,
+    @Param('year') year: string,
+    @Req() req: any,
+  ) {
+    return this.statisticsService.getStatistics(
+      month,
+      year,
+      req.user.tipo === Role.DEVELOPER ? req.user.id : undefined,
     );
   }
 }
